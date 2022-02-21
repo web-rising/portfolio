68bd52f90e1813f1fa2fbf6db43bd6605a667b38
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-02-21 15:37:50 -0500

    big ass changes

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,5 @@
+const tsParticles = require("tsparticles");
+
 window.addEventListener("load", function () {
   const header = document.querySelector("header");
   header.classList.remove("nav");
@@ -7,3 +9,12 @@ window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("nav", window.scrollY > 0);
 });
+
+tsParticles
+  .loadJSON("tsparticles", "presets/default.json")
+  .then((container) => {
+    console.log("callback - tsparticles config loaded");
+  })
+  .catch((error) => {
+    console.error(error);
+  });

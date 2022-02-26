0bc01e0e1203fbbdfeda2079009c3afd44cf6ae6
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-02-25 19:20:23 -0500

    tsparticles
    copied random codepen to get started

--- project/js/main.js
+++ project/js/main.js
@@ -1,20 +1,131 @@
-const tsParticles = require("tsparticles");
+const particleConfig = {
+  backgroundMask: {
+    enable: true,
+    cover: {
+      value: {
+        r: 0,
+        g: 0,
+        b: 0,
+      },
+    },
+  },
+  background: {
+    image: "url('../public/logo.svg')",
+    size: "100% 100%",
+    repeat: "no-repeat",
+  },
+  fullScreen: {
+    enable: true,
+    zIndex: -1,
+  },
+  particles: {
+    color: {
+      value: ["#1E00FF", "#FF0061", "#E1FF00", "#00FF9E"],
+      animation: {
+        enable: true,
+        speed: 30,
+      },
+    },
+    move: {
+      direction: "bottom",
+      enable: true,
+      outModes: {
+        default: "out",
+      },
+      size: true,
+      speed: {
+        min: 1,
+        max: 3,
+      },
+    },
+    number: {
+      value: 700,
+      density: {
+        enable: true,
+        area: 800,
+      },
+    },
+    opacity: {
+      value: 1,
+      animation: {
+        enable: false,
+        startValue: "max",
+        destroy: "min",
+        speed: 0.3,
+        sync: true,
+      },
+    },
+    rotate: {
+      value: {
+        min: 0,
+        max: 360,
+      },
+      direction: "random",
+      move: true,
+      animation: {
+        enable: true,
+        speed: 60,
+      },
+    },
+    tilt: {
+      direction: "random",
+      enable: true,
+      move: true,
+      value: {
+        min: 0,
+        max: 360,
+      },
+      animation: {
+        enable: true,
+        speed: 60,
+      },
+    },
+    shape: {
+      type: ["circle", "square", "polygon"],
+      options: {
+        polygon: [
+          {
+            sides: 5,
+          },
+          {
+            sides: 6,
+          },
+        ],
+      },
+    },
+    size: {
+      value: {
+        min: 3,
+        max: 5,
+      },
+    },
+    roll: {
+      darken: {
+        enable: true,
+        value: 30,
+      },
+      enlighten: {
+        enable: true,
+        value: 30,
+      },
+      enable: true,
+      speed: {
+        min: 15,
+        max: 25,
+      },
+    },
+    wobble: {
+      distance: 30,
+      enable: true,
+      move: true,
+      speed: {
+        min: -15,
+        max: 15,
+      },
+    },
+  },
+};
 
-window.addEventListener("load", function () {
-  const header = document.querySelector("header");
-  header.classList.remove("nav");
+tsParticles.load("tsparticles", particleConfig, function () {
+  console.log("particles loaded");
 });
-
-window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
-  header.classList.toggle("nav", window.scrollY > 0);
-});
-
-tsParticles
-  .loadJSON("tsparticles", "presets/default.json")
-  .then((container) => {
-    console.log("callback - tsparticles config loaded");
-  })
-  .catch((error) => {
-    console.error(error);
-  });

b42d04322dbeb9c01a1fe220c0f2d9796c1fb407
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-04-23 13:22:14 -0400

    apparently it works

--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -4,9 +4,72 @@
       <div class='container'>
         <h2>Built by high school developers — for you, for free.</h2>
       </div>
-      <Particles
-                id='tsparticles'
-                :options="{
+       <Particles id='tsparticles' :options='this.particleOptions'/>
+    </section>
+    <div class='spacer waves1'></div>
+    <section id='ipsum' class='ipsum'>
+       <Particles id='tsparticles' :options='this.particleOptions'/>
+      <!-- <div class='particles' id='tsparticles2'></div> -->
+      <!-- <TSParticles/> -->
+      <div class='container'>
+        <h2>Don't limit your business's potential.</h2>
+        <p>
+          Our team has the resources and technical expertise to craft a
+          streamlined platform.
+        </p>
+      </div>
+    </section>
+    <div class='spacer waves2'></div>
+    <section id='dolor' class='dolor'>
+      <div class='container'>
+        <h2>Tailored for your needs.</h2>
+        <p>
+          Showcase your products, market your business, and sell your services.
+        </p>
+      </div>
+      <Particles id='tsparticles' :options='this.particleOptions'/>
+    </section>
+    <div class='spacer waves3'></div>
+    <section id='sit' class='sit'>
+      <Particles id='tsparticles' :options='this.particleOptions'/>
+      <div class='container'>
+        <h2>Our work.</h2>
+        <p>Check back here shortly for our projects.</p>
+      </div>
+    </section>
+    <div class='spacer waves4'></div>
+    <section id='amet' class='amet'>
+      <div class='container'>
+        <h2>Get involved.</h2>
+        <div class='divider'>
+          <div class='developers'>
+            <h3>For Volunteers</h3>
+            <p>Help us help your community.</p>
+            <Button>Contact Us</Button>
+          </div>
+          <div class='businesses'>
+            <h3>For Businesses</h3>
+            <p>Reach out to us to get started.</p>
+            <Button>Get Started</Button>
+          </div>
+        </div>
+      </div>
+    <Particles id='tsparticles' :options='this.particleOptions'/>
+    </section>
+    <div class='spacer waves5'></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Button from '@/components/Button.vue'
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      particleOptions: {
+
   fpsLimit: 120,
   particles: {
     number: {
@@ -50,70 +113,10 @@
   detectRetina: !0,
   fullScreen: {
     enable: false,
+  }
+      }
+    }
   },
-}"/>
-    </section>
-    <div class='spacer waves1'></div>
-    <section id='ipsum' class='ipsum'>
-      <!-- <div class='particles' id='tsparticles2'></div> -->
-      <!-- <TSParticles/> -->
-      <div class='container'>
-        <h2>Don't limit your business's potential.</h2>
-        <p>
-          Our team has the resources and technical expertise to craft a
-          streamlined platform.
-        </p>
-      </div>
-    </section>
-    <div class='spacer waves2'></div>
-    <section id='dolor' class='dolor'>
-      <div class='container'>
-        <h2>Tailored for your needs.</h2>
-        <p>
-          Showcase your products, market your business, and sell your services.
-        </p>
-      </div>
-      <!-- <div class='particles' id='tsparticles3'></div> -->
-      <!-- <TSParticles/> -->
-    </section>
-    <div class='spacer waves3'></div>
-    <section id='sit' class='sit'>
-      <!-- <div class='particles' id='tsparticles4'></div> -->
-      <!-- <TSParticles/> -->
-      <div class='container'>
-        <h2>Our work.</h2>
-        <p>Check back here shortly for our projects.</p>
-      </div>
-    </section>
-    <div class='spacer waves4'></div>
-    <section id='amet' class='amet'>
-      <div class='container'>
-        <h2>Get involved.</h2>
-        <div class='divider'>
-          <div class='developers'>
-            <h3>For Volunteers</h3>
-            <p>Help us help your community.</p>
-            <Button>Contact Us</Button>
-          </div>
-          <div class='businesses'>
-            <h3>For Businesses</h3>
-            <p>Reach out to us to get started.</p>
-            <Button>Get Started</Button>
-          </div>
-        </div>
-      </div>
-      <!-- <div class='particles' id='tsparticles5'></div> -->
-    </section>
-    <div class='spacer waves5'></div>
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import Button from '@/components/Button.vue'
-
-export default {
-  name: 'HomeView',
   components: {
     Button,
 },
